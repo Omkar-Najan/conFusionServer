58773eefa5c4b0bfe31439ec1cb4c3371d4dacb2
= <=>
2022-09-02 16:46:46 +0530

    Express Rest Api with mongoose part 1

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+// connect app with mongodb database 
+const mongoose = require('mongoose');
+
+
+// imort schema
+const Dishes = require('./models/dishes')
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+  console.log('Connected to mongodb server !');
+  }, 
+  (err) => {console.log(err)}
+);
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
